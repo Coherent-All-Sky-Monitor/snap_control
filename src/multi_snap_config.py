905d6a220e35090c0eb7bef8002519bb77d955e5
liamconnor <liam.dean.connor@gmail.com>
2026-02-11 08:58:39 -0500

    Adding a sleep program. This fixes a "race" issue between casper and casm_f

--- src/multi_snap_config.py
+++ src/multi_snap_config.py
@@ -239,13 +239,10 @@ def _configure_board(board: dict, common: dict,
     )
 
     time.sleep(10)
-    snap.program(fpgfile, initialize_adc=True)
-    exit()
     # Programming the SNAP. This is the main function that programs the SNAP
     # and initializes the ADC.
     try:
         snap.program(fpgfile, initialize_adc=True)
-        print("Finished on attempt %d" % adc_attempts)
     except:
         print("Initial program failed. Attempting to initialize ADC.")
         snap.adc.initialize()
